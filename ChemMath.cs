53b600b70445b4c5181a2a78a8ca81482d19c422
JacobTheChemist <jakub.cervenka@vscht.cz>
2025-01-01 18:50:53 +0100

    Added methods for barebones calculations

---
+++ ChemMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPA_Zkouska
+{
+    internal class ChemMath
+    {
+        public double ToMolarAmount(double weight, double molarMass)
+        {
+            return weight / molarMass;
+        }
+
+        public double ToMolarConcentration(double molarAmount, double volume)
+        {
+            return molarAmount / volume;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace UPA_Zkouska
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
